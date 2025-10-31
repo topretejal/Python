57a1e9dfd74a34e44efe57376c645d92eb49d6b8
Tejal Topre <tejal17trt@gmail.com>
2025-10-31 12:56:15 +0530

    assgn1: task1 and task2

---
+++ assignment1/assgn1_task1.py
@@ -0,0 +1,16 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+#addition
+total = num1 + num2
+#subtraction
+sub = num1-num2
+#multiplication
+mul = num1*num2
+#division
+div = num1/num2
+
+print("Addition:", total)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+
